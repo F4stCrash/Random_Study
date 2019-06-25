13d634aae0edb553c81f47c4cf311102f7ce6613
Fast Crash Games <contact@fastcrash.com>
2019-06-25 14:17:19 -0300

    Initial commit

---
+++ Source/Random/ConvertClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class RANDOM_API ConvertClass
+{
+public:
+	ConvertClass();
+	~ConvertClass();
+};
